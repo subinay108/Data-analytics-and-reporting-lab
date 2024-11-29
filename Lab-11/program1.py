0df7461b4638c0a03157ddb25a551e2054d3f7c5
subinay108 <subinayp108@gmail.com>
2024-11-29 12:01:16 +0530

    lab11 updated

--- Lab-11/program1.py
+++ Lab-11/program1.py
@@ -1,10 +1,14 @@
 # Write a python code to generate a word cloud and find most important words and find frequency
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import re
+from collections import Counter
 
-file = open('transformer.txt', 'r')
+file = open('transformer.txt')
 
 text = file.read()
+
+text = re.sub(r'[,.]', '', text).lower()
 
 # print(text)
 
@@ -14,7 +18,11 @@ plt.imshow(wc, interpolation='bilinear')
 plt.axis('off')
 plt.show()
 
-word_frequencies = wc.words_
+word_values = wc.words_
 
-most_important_word = max(word_frequencies, key=word_frequencies.get)
+most_important_word = max(word_values, key=word_values.get)
 print('Most Important Word: ', most_important_word)
+
+word_frequencies = Counter(text.split())
+
+print(word_frequencies)
